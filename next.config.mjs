e56ad8bbd29a68f6ae132960d74cdd6daa2bfb57
suganoshota <suganoshota@team-lab.com>
2024-10-15 18:16:33 +0900

    これでどうだ..?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
   distDir: "docs",
   experimental: {
     appDir: true,
