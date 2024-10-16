a0f53a29922327efec17d99858d993c17cf79ece
suganoshota <suganoshota@team-lab.com>
2024-10-16 11:24:25 +0900

    本番環境と開発環境でのenvの使い分け

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // build時にssbu-directionがbasePathになるのでローカルでbuild、検証したいときはコメントアウトする
   basePath:
     process.env.NODE_ENV === "production" ? "/ssbu-direction" : undefined,
+  // build時にssbu-directionがassetPrefixになるのでローカルでbuild、検証したいときはコメントアウトする.
   assetPrefix:
     process.env.NODE_ENV === "production" ? "/ssbu-direction" : undefined,
   distDir: "docs",
-  experimental: {
-    appDir: true,
-  },
+  appDir: true,
   reactStrictMode: true,
 };
 
